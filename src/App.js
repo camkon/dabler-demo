37cbd4687d49f7166a7a9316c27d5161cae6eb6e
Ansaf Nisam <80558066+camkon@users.noreply.github.com>
2022-10-11 07:39:11 +0530

    refactor - re-routed the components in elements

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import Layout from './contents'
-import Projects from './contents/Projects'
+import Projects from './components/Projects'
+import Group from './components/Group'
+import Element from './contents/Element'
+import Details from './contents/Element/Details'
+import Code from './contents/Element/Code'
+import Validation from './contents/Element/Validation'
+import BuildHistory from './contents/Element/Build'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}> 
-          <Route path="/projects" element={<Projects />}>
-            <Route />
+          <Route path="/projects" element={<Projects/>}/>
+          <Route path="/projects/:project_name" element={<Group/>}/>
+          <Route path="/projects/:project_name/:warehouse" element={<Element/>}>
+            <Route path="/projects/:project_name/:warehouse/details" element={<Details />}/>
+            <Route path="/projects/:project_name/:warehouse/code" element={<Code />}/>
+            <Route path="/projects/:project_name/:warehouse/validation" element={<Validation />}/>
+            <Route path="/projects/:project_name/:warehouse/build-history" element={<BuildHistory />}/>
           </Route>
         </Route>
       </Routes>
